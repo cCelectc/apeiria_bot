6b33787bec8f4cc59bc9e8a7cd702463ccce4ed4
Cccc_ <me@iscccc.eu.org>
2026-06-27 00:01:30 +0800

    feat(webui): add shadcn-vue components, design tokens and self-hosted fonts

--- webui/src/main.ts
+++ webui/src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
+
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/noto-sans-sc/400.css'
+import '@fontsource/noto-sans-sc/500.css'
+import '@fontsource/noto-sans-sc/700.css'
+
 import './style.css'
 import App from './App.vue'
 
