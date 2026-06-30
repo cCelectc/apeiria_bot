074ad9769ed8116d1e58c8b9619456ddd136c602
Cccc_ <me@iscccc.eu.org>
2026-06-30 11:54:52 +0800

    fix: remove useless eslint-disable

--- webui/src/i18n/index.ts
+++ webui/src/i18n/index.ts
@@ -9,7 +9,6 @@ const i18n = createI18n({
   missingWarn: false,
   missing: (_locale: string, key: string): string => {
     if (import.meta.env.DEV) {
-      // eslint-disable-next-line no-console
       console.warn(`[i18n] missing key: ${key}`);
     }
     return key;
